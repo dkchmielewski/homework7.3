846395a585759f6811375a82e6449e9c2a8d6d8d
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-27 13:56:48 -0400

    Created Game and GuessNumber class

---
+++ src/GuessNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    Scanner scanner = new Scanner(System.in);
+    public void getNumber() {
+        int number;
+        do {
+            System.out.println("Podaj liczbe");
+            number = scanner.nextInt();
+            if (number % 3 == 0 && number > 99 && number < 201) {
+                System.out.println("Liczba jest ok");
+                break;
+            } else if (number < 100) {
+                System.out.println("Liczna jest za mala");
+            } else if (number > 200) {
+                System.out.println("Liczba jest za duza");
+            } else if (number % 3 != 0 && number > 99 && number < 201) {
+                System.out.println("Liczba nie jest podzielna przez 3");
+            }
+        }
+        while (number % 3 != 0 && number > 99 && number < 201 ||  number > 200 || number < 100);
+
+    }
+}
