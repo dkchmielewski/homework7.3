996fb0632567e888246efcfd43cbf86c8c9e44e2
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-28 14:48:52 -0400

    Deleted one if statement at the beginning of do while loop

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -7,15 +7,14 @@ public class GuessNumber {
         do {
             System.out.println("Podaj liczbe");
             number = scanner.nextInt();
-            if (number % 3 == 0 && number > 99 && number < 201) {
-                System.out.println("Liczba jest ok");
-                break;
-            } else if (number < 100) {
+            if (number < 100) {
                 System.out.println("Liczna jest za mala");
             } else if (number > 200) {
                 System.out.println("Liczba jest za duza");
-            } else {
+            } else if (number % 3 != 0 && number > 99 && number < 201) {
                 System.out.println("Liczba nie jest podzielna przez 3");
+            } else {
+                System.out.println("Liczba jest ok");
             }
         }
         while (number % 3 != 0 && number > 99 && number < 201 ||  number > 200 || number < 100);
